e2994705922474e3d95e089386d496c2a2b9c23a
adamkocsis <adam.t.kocsis@gmail.com>
2021-04-27 13:39:02 +0200

    Added number of round check of grab_matrix()

--- R/graphics.R
+++ R/graphics.R
@@ -1,133 +1,73 @@
+buffer <- NULL
 
-#' Code snippets defining ranges based on points located on a plot
+#' Grab a matrix based on interactively input coordinates
 #' 
-#' The function returns snippets of code that you can paste in your script after you select points on a plot. Useful for defining areas on a map. The default methods assume that you will first click in the bottom left and then in the bottom right corner.
+#' The function constructs a function call to create a matrix based on
+#' clicks of the mouse
 #' 
-#' @param round (\code{integer}) Number of digits to round to, can be two values, first is for \code{x} second for \code{y}. 
-#' @param f (\code{character}) A single letter value specifying for which function's arugment format you want to get parameters. \code{"p"} is for \code{plot}, \code{"r"} is for \code{\link[graphics]{rect}}, \code{"s"} is for \code{\link[graphics]{segments}}. \code{"e"} returns a call to create an \code{\link[raster]{extent}} class object from the package \code{raster}. \code{"m"} will return code to define a 2 column matrix.
-#' @param n (\code{integer}) The number of points to request. 
-#' @param ... arguments passed to the \code{\link[graphics]{locator}} function
-#' @return For certain methods (\code{"m"} and \code{"e"}) the function returns a \code{matrix} or \code{extent} class object if the function output is assigned to a name.
-#' @examples
-#' \donttest{
-#' # plot something
-#' data(dems)
-#' mapplot(dems[1], col="earth")
-#' # click 5 times to get the long-lat coords of 5 points
-#' # shaper("m",5)
-#' # example output:
-#' mat <- matrix(c(								
-#'	 -2.89, 31.55,
-#'	 3.32, 26.99,
-#'	 21.17, 17.87,
-#'	 33.6, 11.03,
-#'	 5.65, 19.39
-#' ), ncol=2, byrow=TRUE)
-#' #plot them 
-#' points(mat)
-#'}
+#' This is a wrapper to the locator() function, quickening the
+#' definition of matrices based on plots.
+#' @param n \code{numeric} Positive integer, the number of coordinates to be located
+#' @param round \code{numeric} Positive integer, the number of digits to conserve.
+#' @param ... Arguments passed to the locator() function.
+#' @return The function returns a numeric matrix when assigned to an object.
 #' @export
-shaper <- function(f="p",n=2, round=2,...){
-	if(length(round)==1) round <- rep(round,2)
+grab_matrix <- function(n=2, round=2,...){
 	
-	if(n==2){
-		if(length(f)==1 & any(f=="s")){
-			cat("Click on the first point:				\r")
-			p1 <- graphics::locator(1,...)
-			flush.console()
-			
-			cat("Click on the second point:			\r")
-			p2 <- graphics::locator(1,...)
-			flush.console()
-		}else{
-			cat("Click in bottom-left corner:		\r")
-			p1 <- graphics::locator(1, ...)
-			flush.console()
-			
-			cat("Click in top-right corner:			\r")
-			p2 <- graphics::locator(1,...)
-			flush.console()
-		}
-		
-		x1<- p1$x
-		y1<- p1$y
-		x2<- p2$x
-		y2<- p2$y
-		
-		x1 <- round(x1, round[1])
-		y1 <- round(y1, round[2])
-		x2 <- round(x2, round[1])
-		y2 <- round(y2, round[2])
-		
-		
+	if(length(round)==1) {
+		round <- rep(round,2)
 	}else{
-		if(any(f=="s")) stop("You cannot get a segments() call for multiple points.")
-		
-		cat("Select", n, "points:\r")
-		loc <- graphics::locator(n, ...)
-		
-		xs<-loc$x
-		ys<-loc$y
-		
-		x1 <- round(min(xs), round[1])
-		y1 <- round(min(ys), round[2])
-		x2 <- round(max(xs), round[1])
-		y2 <- round(max(ys), round[2])
+		# do a check, make sure the length is ok
+		if(length(round)!=n) stop("Incorrect number of rounding values.")
+	}
+
+	# use the buffer
+	buffer$xs <- NULL
+	buffer$ys <- NULL
+
+	# grab the points one by one
+	for(i in 1:n){
+		cat("Click on  point no. ",i,":				\r")
+		point <- graphics::locator(1,...)
+		buffer$xs <- c(buffer$xs, point$x)
+		buffer$ys <- c(buffer$ys, point$y)
+		flush.console()
 		
 	}
+	
 	# clean the console
-	cat("																\r")
+	cat("					               					\r")
 	flush.console()
-	
-	# stop if the two are the same
-	
+		
 	# results
 	res <- NULL
 	
-	if(any(f=="p")){
-		cat(paste("xlim=c(", deparse(x1),", ", deparse(x2), "), ylim=c(", deparse(y1),", ", deparse(y2), ")", sep=""), "\n")
-	}
 	
-	if(any(f=="r")){
-		cat(paste("xleft=", deparse(x1), ", xright=", deparse(x2), ", ybottom=", deparse(y1), ", ytop=", deparse(y2), sep=""), "\n")
-	}
+	# display output
+	cat("mat <- matrix(c(", "\n")
 	
-	if(any(f=="s")){
-		cat(paste("x0=", deparse(x1), ", x1=", deparse(x2), ", y1=", deparse(y1), ", y2=", deparse(y2), ")", sep=""), "\n")
-	}
-	
-	if(any(f=="e")){
-		xmin <- min(c(x1, x2))
-		xmax <- max(c(x1, x2))
-		ymin <- min(c(y1, y2))
-		ymax <- max(c(y1, y2))
-		
-		cat("ext <- extent(c(", "\n")
-		cat(paste("	xmin = ", deparse(xmin), ",\n", sep=""))
-		cat(paste("	xmax = ", deparse(xmax), ",\n", sep=""))
-		cat(paste("	ymin = ", deparse(ymin), ",\n", sep=""))
-		cat("	ymax =", deparse(ymax), "\n")
-		cat("))", "\n")
-		res <- raster::extent(c(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax))
-	}
-	
-	if(any(f=="m")){
-		cat("mat <- matrix(c(", "\n")
-		
-		if(!is.null(round)) xs<-round(xs, round)
-		if(!is.null(round)) ys<-round(ys, round)
-		for(i in 1:n){
-			if(i!=n){
-				cat(paste("	", deparse(xs[i]),", ", deparse(ys[i]), ",\n", sep=""))
-			}else{
-				cat(paste("	", deparse(xs[i]),", ", deparse(ys[i]), "\n", sep=""))
-			}
-			
+	if(!is.null(round)) xs<-round(buffer$xs, round)
+	if(!is.null(round)) ys<-round(buffer$ys, round)
+	for(i in 1:n){
+		if(i!=n){
+			cat(paste("	", deparse(xs[i]),", ", deparse(ys[i]), ",\n", sep=""))
+		}else{
+			cat(paste("	", deparse(xs[i]),", ", deparse(ys[i]), "\n", sep=""))
 		}
 		
-		cat("), ncol=2, byrow=TRUE)", "\n")
-		res <- matrix(c(xs, ys), ncol=2)
 	}
+		
+	cat("), ncol=2, byrow=TRUE)", "\n")
+	res <- matrix(c(xs, ys), ncol=2)
+
 	invisible(res)
 	
 }
+
+
+
+grab_lims(round=2,...){
+	if(length(round)==1) round <- rep(round,2)
+}
+		
+
